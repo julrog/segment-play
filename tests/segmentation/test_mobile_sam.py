665e138733179e4acb8f95ff8c5dfad639a63844
julrog <julianrogawski@gmail.com>
2024-02-11 20:41:43 +0100

    add ai tests for segmentation

--- tests/segmentation/test_mobile_sam.py
+++ tests/segmentation/test_mobile_sam.py
@@ -5,7 +5,11 @@ from frame.producer import FrameData
 from pipeline.data import DataCollection
 from pose.producer import PoseData
 from segmentation.mobile_sam import MobileSam
+from tests.ai_tester import AITester
+from tests.conftest import requires_env
 from tracking.producer import TrackingData
+from util.image import scale_image
+from util.mask import apply_mask
 
 
 @pytest.fixture
@@ -60,3 +64,30 @@ def test_bbox_masks_pose(
     )
     assert isinstance(masks, np.ndarray)
     assert len(masks) > 0
+
+
+@requires_env('ai_tests')
+def test_segmentation_ai(
+    ai_tester: AITester,
+    mobile_sam: MobileSam,
+    track_data_collection: DataCollection
+) -> None:
+    bb = track_data_collection.get(TrackingData).get_box(0)
+    points = None
+    point_modes = None
+    mobile_sam.set_image(track_data_collection.get(FrameData).get_frame())
+    masks = mobile_sam.bbox_masks(bb, points, point_modes)
+
+    image = track_data_collection.get(FrameData).get_frame()
+    black_image = np.zeros_like(image)
+    masked_image = apply_mask(black_image.copy(), image, masks[0])
+    reversed_masked_image = apply_mask(image.copy(), black_image, masks[0])
+
+    assert not ai_tester.binary_question(
+        scale_image(image, 0.1), 'Is there cut out person on a black background?')  # noqa: E501
+    assert ai_tester.binary_question(
+        scale_image(masked_image, 0.1), 'Is there cut out person on a black background?')  # noqa: E501
+    assert not ai_tester.binary_question(
+        scale_image(image, 0.1), 'Is there a black silhouette in the picture?')  # noqa: E501
+    assert ai_tester.binary_question(
+        scale_image(reversed_masked_image, 0.1), 'Is there a black silhouette in the picture?')  # noqa: E501
--- tests/segmentation/test_sam.py
+++ tests/segmentation/test_sam.py
@@ -5,7 +5,11 @@ from frame.producer import FrameData
 from pipeline.data import DataCollection
 from pose.producer import PoseData
 from segmentation.sam import Sam
+from tests.ai_tester import AITester
+from tests.conftest import requires_env
 from tracking.producer import TrackingData
+from util.image import scale_image
+from util.mask import apply_mask
 
 
 @pytest.fixture
@@ -57,3 +61,30 @@ def test_bbox_masks_pose(
     )
     assert isinstance(masks, np.ndarray)
     assert len(masks) > 0
+
+
+@requires_env('ai_tests')
+def test_segmentation_ai(
+    ai_tester: AITester,
+    sam: Sam,
+    track_data_collection: DataCollection
+) -> None:
+    bb = track_data_collection.get(TrackingData).get_box(0)
+    points = None
+    point_modes = None
+    sam.set_image(track_data_collection.get(FrameData).get_frame())
+    masks = sam.bbox_masks(bb, points, point_modes)
+
+    image = track_data_collection.get(FrameData).get_frame()
+    black_image = np.zeros_like(image)
+    masked_image = apply_mask(black_image.copy(), image, masks[0])
+    reversed_masked_image = apply_mask(image.copy(), black_image, masks[0])
+
+    assert not ai_tester.binary_question(
+        scale_image(image, 0.1), 'Is there cut out person on a black background?')  # noqa: E501
+    assert ai_tester.binary_question(
+        scale_image(masked_image, 0.1), 'Is there cut out person on a black background?')  # noqa: E501
+    assert not ai_tester.binary_question(
+        scale_image(image, 0.1), 'Is there a black silhouette in the picture?')  # noqa: E501
+    assert ai_tester.binary_question(
+        scale_image(reversed_masked_image, 0.1), 'Is there a black silhouette in the picture?')  # noqa: E501
